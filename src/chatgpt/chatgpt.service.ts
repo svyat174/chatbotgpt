9a0fcf4f898fc364dc2644cf8f77e5511f485eba
Svyatoslav Glukhov <svyatgluhov@gmail.com>
2023-04-26 17:40:32 +0300

    ADDED: chatGptService and add onMessage

--- src/chatgpt/chatgpt.service.ts
+++ src/chatgpt/chatgpt.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ChatgptModule } from './chatgpt.module';
+import { Observable, catchError, map, of } from 'rxjs';
+import { IGptAnswer } from './interfaces/gpt-answer.interface';
 
 @Injectable()
-export class ChatgptService {}
+export class ChatgptService {
+  private readonly logger = new Logger(ChatgptModule.name);
+  private gptUrl: string;
+  private apiKey: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.gptUrl = this.configService.get<string>('GPT_URL');
+    this.apiKey = this.configService.get<string>('GPT_API');
+  }
+
+  generateResponse(content: string): Observable<string> {
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.apiKey}`,
+    };
+    const data = {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content }],
+      temperature: 1,
+    };
+    return this.httpService
+      .post<IGptAnswer>(this.gptUrl, data, { headers })
+      .pipe(
+        map(({ data }) => data.choices[0].message.content.trim()),
+        catchError((err) => {
+          this.logger.error(err);
+          return of('Произошла ошибка');
+        }),
+      );
+  }
+}
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,11 +1,25 @@
-import { Ctx, Start, Update } from 'nestjs-telegraf';
+import { Ctx, Message, On, Start, Update } from 'nestjs-telegraf';
 import { Telegraf } from 'telegraf';
 import { Context } from './interface/context.interfce';
+import { ChatgptService } from 'src/chatgpt/chatgpt.service';
+import { ConfigService } from '@nestjs/config';
 
 @Update()
 export class TelegramService extends Telegraf<Context> {
+  constructor(
+    private readonly chatgptService: ChatgptService,
+    private readonly configService: ConfigService,
+  ) {
+    super(configService.get('TELEGRAM_API'));
+  }
+
   @Start()
   onStart(@Ctx() ctx: Context) {
     ctx.reply(`Hello, ${ctx.from.first_name}!`);
+  }
+
+  @On('text')
+  onMessage(@Message('text') text: string) {
+    return this.chatgptService.generateResponse(text);
   }
 }
